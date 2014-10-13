3d217092556f924d77cb974b7caae52ff3666a90
Andy Eschbacher <ohasselblad@users.noreply.github.com>
2014-10-13 12:23:04 -0400

    Enforcing no onload

--- js_demo2/demo.js
+++ js_demo2/demo.js
@@ -1,24 +1,19 @@
-function main() {
+vzj = 'http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json';
 
-  vzj = 'http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json';
-
-  // Create map object with Leaflet
-  var map = new L.Map('map', {
-    zoomControl: false,
-    center: [43,0], // Southern France
-    zoom: 5
-  });
+// Create map object with Leaflet
+var map = new L.Map('map', {
+  zoomControl: false,
+  center: [43,0], // Southern France
+  zoom: 5
+});
             
-  // Add OpenStreetMap tiles
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'OpenStreetMap'
-  }).addTo(map);
+// Add OpenStreetMap tiles
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'OpenStreetMap'
+}).addTo(map);
 
-  cartodb.createLayer(map, vzj)
-    .addTo(map)
-    .done(function() {
-      alert("Layer loaded");
-    });
-}
-
-window.onload = main;
+cartodb.createLayer(map, vzj)
+  .addTo(map)
+  .done(function() {
+    alert("Layer loaded");
+  });
