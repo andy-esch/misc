6b260eaaa1c6c29c085d3a971d4331b8979542cf
Andy Eschbacher <ohasselblad@users.noreply.github.com>
2014-10-15 12:05:33 -0400

    Update demo.js

--- js_demo/demo.js
+++ js_demo/demo.js
@@ -1,31 +1,16 @@
-            // Create map object with Leaflet
-            var map = new L.Map('map', {
-                zoomControl: false,
-                center: [43,0], // Southern France
-                zoom: 5
-            });
+// Create map object with Leaflet
+var map = new L.Map('map', {
+    zoomControl: false,
+    center: [43,0], // Southern France
+    zoom: 5
+});
             
-            // Add OpenStreetMap tiles
-            L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: 'OpenStreetMap'
-                }).addTo(map);
+// Add OpenStreetMap tiles
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'OpenStreetMap'
+    }).addTo(map);
 
-            vzj = 'http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json';
+vzj = 'http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json';
 
+cartodb.createLayer(map,vzj).addTo(map);
 
-            cartodb.createLayer(map,vzj).addTo(map);
-            // var vizjsons = [
-            //     'http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json',
-            //     'http://documentation.cartodb.com/api/v2/viz/236085de-ea08-11e2-958c-5404a6a683d5/viz.json'
-            //     ]
-
-            // vizjsons.forEach(function(vizjson, index) {
-            //     cartodb.createLayer(map, vizjson)
-            //         .addTo(map)
-            //         .done(function(layer) {
-            //             alert("Congrats, you added vizjson #" + (index+1));
-            //         })
-            //         .error(function(err) {
-            //             console.log("error: " + err + " for layer " + index);
-            //         });
-            //     });
